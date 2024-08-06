110efc0ab8e337cfa444bf98b49c62870f3656c6
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-05 20:40:21 -0700

    Clean up search bar

--- src/lib/components/datatable/Search.svelte
+++ src/lib/components/datatable/Search.svelte
@@ -4,9 +4,26 @@
     let value: string;
 </script>
 
-<input
-    class="input sm:w-64 w-36"
-    type="search"
-    placeholder="Search..."
-    bind:value
-    on:input={() => handler.search(value)} />
+<div
+    class="input-group input-group-divider grid-cols-[auto_1fr] w-40 sm:w-64 lg:w-80">
+    <div class="input-group-shim">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-4">
+            <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+        </svg>
+    </div>
+    <input
+        class="input p-2 border-none outline-none bg-transparent focus:bg-transparent hover:bg-transparent"
+        type="search"
+        placeholder="Search..."
+        bind:value
+        on:input={() => handler.search(value)} />
+</div>
