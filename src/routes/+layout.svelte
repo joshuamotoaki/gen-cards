526c50dd16d964617ddbbba00566ff570392c0cd
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-05 21:14:41 -0700

    Create database context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
+    import { onMount, setContext } from "svelte";
     import "../app.postcss";
+    import Database from "@tauri-apps/plugin-sql";
+
+    onMount(async () => {
+        const db = await Database.load("sqlite:sqlite-1.db");
+        setContext("db", db);
+    });
 </script>
 
 <slot />
