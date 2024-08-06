9715d7903bcaab3208318fea0c3b67eccac5f91e
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-06 10:12:25 -0700

    Move modal logic to the root layout

--- src/routes/(dashboard)/+layout.svelte
+++ src/routes/(dashboard)/+layout.svelte
@@ -1,46 +1,8 @@
 <script lang="ts">
-    import { onMount } from "svelte";
     import Sidebar from "./Sidebar.svelte";
-    import { SyncLoader } from "svelte-loading-spinners";
-    import {
-        initializeStores,
-        Modal,
-        type ModalComponent
-    } from "@skeletonlabs/skeleton";
-
-    // Modal Imports
-    import ModalExampleForm from "$lib/components/modals/ModalExampleForm.svelte";
-
-    // Modal Components
-    initializeStores();
-    const modalRegistry: Record<string, ModalComponent> = {
-        settings: { ref: ModalExampleForm }
-    };
-
-    // Initial loading state
-    let isReady = false;
-    onMount(async () => {
-        setTimeout(() => {
-            isReady = true;
-        }, 0);
-    });
 </script>
 
-<Modal components={modalRegistry} />
 <div class="flex h-screen overflow-hidden">
-    {#if isReady}
-        <Sidebar />
-        <slot />
-    {:else}
-        <div
-            class="flex items-center flex-col justify-center w-full h-full gap-4">
-            <div class="flex items-center gap-4">
-                <img src="logo.webp" class="h-24 w-24" alt="GenCards Logo" />
-                <h1 class="text-7xl font-light">GenCards</h1>
-            </div>
-            <div>
-                <SyncLoader color="#DA2602" />
-            </div>
-        </div>
-    {/if}
+    <Sidebar />
+    <slot />
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,52 @@
-<script>
+<script lang="ts">
     import { onMount, setContext } from "svelte";
     import "../app.postcss";
     import Database from "@tauri-apps/plugin-sql";
     import { currentTheme } from "$lib/config";
     import { browser } from "$app/environment";
+    import { initializeStores, Modal } from "@skeletonlabs/skeleton";
+    import type { ModalComponent } from "@skeletonlabs/skeleton";
 
+    // Modal imports
+    import Settings from "$lib/components/modals/Settings.svelte";
+    import { SyncLoader } from "svelte-loading-spinners";
+
+    // Theme change
     $: {
         if (browser) {
             document.body.setAttribute("data-theme", $currentTheme);
         }
     }
 
+    // Modal Components
+    initializeStores();
+    const modalRegistry: Record<string, ModalComponent> = {
+        settings: { ref: Settings }
+    };
+
+    let isReady = false;
+
     onMount(async () => {
-        const db = await Database.load("sqlite:sqlite-1.db");
-        setContext("db", db);
+        // const db = await Database.load("sqlite:sqlite-1.db");
+        // setContext("db", db);
+
+        setTimeout(() => {
+            isReady = true;
+        }, 0);
     });
 </script>
 
-<slot />
+<Modal components={modalRegistry} />
+{#if isReady}
+    <slot />
+{:else}
+    <div class="flex items-center flex-col justify-center h-screen gap-4">
+        <div class="flex items-center gap-4">
+            <img src="logo.webp" class="h-24 w-24" alt="GenCards Logo" />
+            <h1 class="text-7xl font-light">GenCards</h1>
+        </div>
+        <div>
+            <SyncLoader color="#DA2602" />
+        </div>
+    </div>
+{/if}
