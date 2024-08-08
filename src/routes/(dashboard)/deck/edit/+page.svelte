a60160f9f835db3aa2a468bf7339a1dc5ebab3eb
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-08 16:58:44 -0700

    Add card upload button

--- src/routes/(dashboard)/deck/edit/+page.svelte
+++ src/routes/(dashboard)/deck/edit/+page.svelte
@@ -93,7 +93,7 @@
 
         <form>
             <label class="label space-y-0 mb-2">
-                <span class="text-lg"> Title </span>
+                <span class="text-lg font-semibold"> Title </span>
                 <input
                     value={$currentDeck.info.title}
                     on:input={e => {
@@ -107,7 +107,7 @@
                     placeholder="Input a title" />
             </label>
             <label class="label space-y-0 mb-2">
-                <span class="text-lg"> Description </span>
+                <span class="text-lg font-semibold"> Description </span>
                 <textarea
                     value={$currentDeck.info.description}
                     on:input={e => {
@@ -123,9 +123,27 @@
 
             <!-- Cards-->
             <section class="mb-4">
-                <h2 class="text-lg mb-2">
-                    Cards ({$currentDeck.cards.cards.length})
-                </h2>
+                <div class="flex justify-between items-center mb-2">
+                    <h2 class="text-lg font-semibold">
+                        Cards ({$currentDeck.cards.cards.length})
+                    </h2>
+                    <button class="btn btn-sm variant-filled-secondary gap-1">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke-width="1.5"
+                            stroke="currentColor"
+                            class="size-6">
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15m0-3-3-3m0 0-3 3m3-3V15" />
+                        </svg>
+
+                        Upload Cards
+                    </button>
+                </div>
                 <div>
                     <div>
                         {#each $currentDeck.cards.cards as card, index}
