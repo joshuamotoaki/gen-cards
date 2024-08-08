1c3233f39b14f2538b5568cceb35270dc80f7a26
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-07 19:30:54 -0700

    Create generic persisted writable

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,22 @@
 import { writable } from "svelte/store";
 import type { Theme } from "./types";
 
-export const currentTheme = writable<Theme>("rocket");
+const createPersistedWritable = <T>(key: string, initialValue: T) => {
+    const store = writable<T>(
+        JSON.parse(localStorage.getItem(key) || JSON.stringify(initialValue))
+    );
+
+    return {
+        subscribe: store.subscribe,
+        update: store.update,
+        set: (value: T) => {
+            localStorage.setItem(key, JSON.stringify(value));
+            store.set(value);
+        }
+    };
+};
+
+export const currentTheme = createPersistedWritable<Theme>(
+    "currentTheme",
+    "rocket"
+);
