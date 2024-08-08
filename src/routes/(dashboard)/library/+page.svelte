07f29392ab6c56c784f5a029bc59880a84ee8009
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-07 19:40:20 -0700

    Show message for library if no decks

--- src/routes/(dashboard)/library/+page.svelte
+++ src/routes/(dashboard)/library/+page.svelte
@@ -1,49 +1,76 @@
 <script lang="ts">
     import { DataHandler } from "@vincjo/datatables";
-    import { SAMPLE_DECKS } from "$lib/samples";
     import Search from "$lib/components/datatable/Search.svelte";
     import ThSort from "$lib/components/datatable/ThSort.svelte";
     import RowCount from "$lib/components/datatable/RowCount.svelte";
     import Pagination from "$lib/components/datatable/Pagination.svelte";
+    import { decks } from "$lib/state";
+    import { createNewDeck } from "$lib/helpers";
 
-    const handler = new DataHandler(SAMPLE_DECKS, { rowsPerPage: 10 });
+    const handler = new DataHandler($decks, { rowsPerPage: 10 });
     const rows = handler.getRows();
 </script>
 
 <div class="flex-1 p-4 overflow-auto">
     <section>
-        <h2 class="text-2xl font-semibold">All Decks</h2>
+        <h2 class="text-2xl mb-2">All Decks</h2>
 
-        <div class="table-container rounded-none space-y-4">
-            <div class="flex justify-between">
-                <Search {handler} />
-            </div>
-            <table class="table table-hover table-compact table-auto w-full">
-                <thead>
-                    <tr>
-                        <ThSort {handler} orderBy="title">Title</ThSort>
-                        <ThSort {handler} orderBy="totalCards"
-                            >Total Cards</ThSort>
-                        <ThSort {handler} orderBy="lastStudied"
-                            >Last Studied</ThSort>
-                    </tr>
-                </thead>
-                <tbody>
-                    {#each $rows as row}
+        {#if $decks.length === 0}
+            <p class="text-lg mb-2">
+                No decks found. Create a new deck to get started!
+            </p>
+            <button
+                class="btn variant-filled-success flex gap-2 items-center"
+                on:click={createNewDeck}>
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke-width="1.5"
+                    stroke="currentColor"
+                    class="size-6">
+                    <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+                New Deck
+            </button>
+        {:else}
+            <div class="table-container rounded-none space-y-4">
+                <div class="flex justify-between">
+                    <Search {handler} />
+                </div>
+                <table
+                    class="table table-hover table-compact table-auto w-full">
+                    <thead>
                         <tr>
-                            <td>{row.title}</td>
-                            <td>{row.totalCards}</td>
-                            <td>
-                                {new Date(row.lastStudied).toLocaleDateString()}
-                            </td>
+                            <ThSort {handler} orderBy="title">Title</ThSort>
+                            <ThSort {handler} orderBy="totalCards"
+                                >Total Cards</ThSort>
+                            <ThSort {handler} orderBy="lastStudied"
+                                >Last Studied</ThSort>
                         </tr>
-                    {/each}
-                </tbody>
-            </table>
-            <div class="flex justify-between">
-                <RowCount {handler} />
-                <Pagination {handler} />
+                    </thead>
+                    <tbody>
+                        {#each $rows as row}
+                            <tr>
+                                <td>{row.title}</td>
+                                <td>{row.card_count}</td>
+                                <td>
+                                    {new Date(
+                                        row.studied_at
+                                    ).toLocaleDateString()}
+                                </td>
+                            </tr>
+                        {/each}
+                    </tbody>
+                </table>
+                <div class="flex justify-between">
+                    <RowCount {handler} />
+                    <Pagination {handler} />
+                </div>
             </div>
-        </div>
+        {/if}
     </section>
 </div>
