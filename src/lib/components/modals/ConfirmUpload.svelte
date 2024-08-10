433cbee908292cdc6196d934fc210f901cdb3d65
Joshua Lau <dachshunduniverse@gmail.com>
2024-08-10 09:59:18 -0700

    Start building out upload confirm display

--- src/lib/components/modals/ConfirmUpload.svelte
+++ src/lib/components/modals/ConfirmUpload.svelte
@@ -1,20 +1,34 @@
 <script lang="ts">
   import { getModalStore } from "@skeletonlabs/skeleton";
+  import { currentDeck, fieldsToAdd } from "$lib/utils/state";
 
   const modalStore = getModalStore();
 
   const onFormSubmit = async () => {
     modalStore.close();
   };
+
+  $: gridCSS = `grid-template-columns: repeat(${$fieldsToAdd && $fieldsToAdd.length}, 1fr);`;
 </script>
 
 {#if $modalStore[0]}
   <div class="card p-4 w-modal shadow-xl">
     <header class="text-2xl font-semibold mb-1">Cards to Add</header>
-    <article class="mb-4">Confirm adding these cards to the deck?</article>
+    <div class="mb-4">Confirm adding these cards to the deck?</div>
     <form on:submit|preventDefault={onFormSubmit}>
-      <div>Hello, World!</div>
-
+      {#if $fieldsToAdd && $fieldsToAdd.length > 0 && $currentDeck}
+        <main>
+          {#each $fieldsToAdd as cardFields, index}
+            <article style={gridCSS} class="grid">
+              {#each $currentDeck?.cards.schema.fields as field}
+                {cardFields[field]}
+              {/each}
+            </article>
+          {/each}
+        </main>
+      {:else}
+        <p>Error: No cards to add.</p>
+      {/if}
       <footer class="flex justify-end gap-2">
         <button
           type="button"
