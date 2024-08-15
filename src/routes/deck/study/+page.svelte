d092e20b3df4cf422fb7868881aeeafa5beec7eb
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-14 17:47:20 -0700

    Protect study route

--- src/routes/deck/study/+page.svelte
+++ src/routes/deck/study/+page.svelte
@@ -1,1 +1,10 @@
-<div>Study</div>
+<script>
+  import { currentDeck } from "$lib/utils/state";
+  import DeckWarning from "../../(dashboard)/deck/DeckWarning.svelte";
+</script>
+
+{#if !$currentDeck}
+  <DeckWarning />
+{:else}
+  <div>Study</div>
+{/if}
