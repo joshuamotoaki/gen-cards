169e8f2417a4b8c755f93b8962759dd17e4436d4
joshuamotoaki <dachshunduniverse@gmail.com>
2024-08-18 20:47:06 -0600

    Fix import

--- src/lib/utils/state.ts
+++ src/lib/utils/state.ts
@@ -1,5 +1,5 @@
 import { get, writable } from "svelte/store";
-import type { Deck, DeckInfo } from "./types";
+import type { Deck, DeckInfo } from "./deck";
 import { db } from "./db";
 
 // All the deck infos.
